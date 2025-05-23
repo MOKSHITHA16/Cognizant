874c632b79c32da46f3aed2146f7be325749a611
MOKSHITHA16 <mokshithareddym16@gmail.com>
2025-05-23 10:01:31 +0530

    Update Operator Precedence.java

--- corejava/Operator Precedence.java
+++ corejava/Operator Precedence.java
@@ -4,8 +4,8 @@ public class OperatorPrecedence {
         int result2 = (10 + 5) * 2;
         int result3 = 20 / 2 + 3 * 4 - 1;
 
-        System.out.println("Result 1 (10 + 5 * 2): " + result1); // 10 + (5*2) = 20
-        System.out.println("Result 2 ((10 + 5) * 2): " + result2); // (10+5)*2 = 30
-        System.out.println("Result 3 (20 / 2 + 3 * 4 - 1): " + result3); // 10 + 12 - 1 = 21
+        System.out.println("Result 1 (10 + 5 * 2): " + result1);
+        System.out.println("Result 2 ((10 + 5) * 2): " + result2); 
+        System.out.println("Result 3 (20 / 2 + 3 * 4 - 1): " + result3);
     }
 }
