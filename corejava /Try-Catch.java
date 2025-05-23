5c3bc353ef18a38c7d0376dd921a9244c042d035
MOKSHITHA16 <mokshithareddym16@gmail.com>
2025-05-23 10:47:31 +0530

    Create Try-Catch.java

---
+++ corejava /Try-Catch.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter the first integer: ");
+            int num1 = scanner.nextInt();
+
+            System.out.print("Enter the second integer: ");
+            int num2 = scanner.nextInt();
+
+            int result = num1 / num2;
+            System.out.println("Result: " + result);
+
+        } catch (ArithmeticException e) {
+            System.out.println(" You can’t divide by zero, babe. Try again with a different number.");
+        }
+
+        scanner.close();
+    }
+}
