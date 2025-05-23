8ee7028c78b409b617972da9edfb3cbf354998bf
MOKSHITHA16 <mokshithareddym16@gmail.com>
2025-05-23 09:19:07 +0530

    Create Array Sum and Average.java

---
+++ corejava/Array Sum and Average.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ArraySumAvg {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many numbers? ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        int sum = 0;
+
+        System.out.println("Enter " + n + " numbers:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        double avg = (double) sum / n;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + avg);
+
+        sc.close();
+    }
+}
